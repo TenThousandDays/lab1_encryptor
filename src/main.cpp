bc03dd127f96d74b3d070c3a41d8730f11fce271
TenThousandDays <127685673+TenThousandDays@users.noreply.github.com>
2026-05-11 20:45:18 +0700

    feat(cli): add arguments parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,50 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
+
+namespace {
+
+enum class Mode
+{
+    Encrypt,
+    Decrypt
+};
+
+void printUsage(const char* executableName)
+{
+    std::cerr << "Usage:\n"
+              << "  " << executableName << " encrypt <directory_path> <password>\n"
+              << "  " << executableName << " decrypt <directory_path> <password>\n\n"
+              << "Examples:\n"
+              << "  " << executableName << " encrypt ./data \"StrongPassword123!\"\n"
+              << "  " << executableName << " decrypt ./data \"StrongPassword123!\"\n";
+}
+
+bool parseMode(const std::string& value, Mode& mode)
+{
+    if (value == "encrypt") {
+        mode = Mode::Encrypt;
+        return true;
+    }
+    if (value == "decrypt") {
+        mode = Mode::Decrypt;
+        return true;
+    }
+    return false;
+}
+
+} // namespace
 
 int main(int argc, char* argv[])
 {
     if (argc != 4) {
-        std::cerr << "Usage:\n"
-                  << "  " << argv[0] << " encrypt <directory_path> <password>\n"
-                  << "  " << argv[0] << " decrypt <directory_path> <password>\n";
+        printUsage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    Mode mode = Mode::Encrypt;
+    if (!parseMode(argv[1], mode)) {
+        printUsage(argv[0]);
         return EXIT_FAILURE;
     }
 
